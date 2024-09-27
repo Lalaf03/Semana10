ebc44af44d8c4a14c29be941a12307ef3899a897
Lalaf03 <laura.fernandez_gom@uao.edu.co>
2024-09-26 21:23:53 -0500

    Actualización

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Cards from "./assets/componentes/Cards.jsx";
-import Video from "./assets/componentes/card/Video.jsx";
+import Video from "./assets/componentes/card/video.jsx";
 import "./App.css";
 
 function App() {
@@ -12,26 +12,7 @@ function App() {
     <>
       <Cards />
       <Video />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Laura Fernández</h1>
     </>
   );
 }
